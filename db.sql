b38803f5f1aa62a8f6d5568f3ef44c932d5bc40f
song-kangfei <2802648097@qq.com>
2026-03-09 21:26:18 +0800

    修改配置，修改项目名称

--- db.sql
+++ db.sql
@@ -1,10 +1,10 @@
 # 1. 初始化脚手架相关的业务表
 # 注意: 此前如果修改过脚手架业务数据库名称，此处需确保名称一致
-create database if not exists frameworkjava_prd charset utf8mb4;
+create database if not exists house_platform charset utf8mb4;
 
 SET NAMES utf8mb4;
 
-use `frameworkjava_prd`;
+use `house_platform`;
 
 CREATE TABLE `sys_region` (
                               `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键id',
@@ -4440,3 +4440,143 @@ INSERT INTO sys_user (nick_name,phone_number,password,`identity`,remark,status) 
     ('admin','e64c5f44dc95e4ca77d99136ea2c88c6','15e2b0d3c33891ebb0f1ef609ec419420c20e320ce94c65fbc8c3312448eb225','super_admin',NULL,'enable');
 # --
 commit ;
+
+DROP TABLE IF EXISTS `house`;
+CREATE TABLE `house`  (
+                          `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+                          `user_id` bigint(20) UNSIGNED NOT NULL COMMENT '房东id',
+                          `title` varchar(50) NOT NULL COMMENT '标题',
+                          `rent_type` varchar(20) NOT NULL COMMENT '租房类型',
+                          `floor` int(11) NOT NULL COMMENT '所在楼层',
+                          `all_floor` int(11) NOT NULL COMMENT '总楼层',
+                          `house_type` varchar(20) NOT NULL COMMENT '户型',
+                          `rooms` varchar(20) NOT NULL COMMENT '居室',
+                          `position` varchar(20) NOT NULL COMMENT '朝向',
+                          `area` decimal(10, 2) NOT NULL COMMENT '面积（平方米）',
+                          `price` decimal(10, 2) NOT NULL COMMENT '价格（元）',
+                          `intro` varchar(2047) NOT NULL COMMENT '房屋介绍',
+                          `devices` varchar(255) NOT NULL COMMENT '设备',
+                          `head_image` varchar(110) NOT NULL COMMENT '头图',
+                          `images` varchar(2047) NULL DEFAULT NULL COMMENT '房源图',
+                          `city_id` bigint(20) NOT NULL COMMENT '城市id',
+                          `city_name` varchar(40) NOT NULL COMMENT '城市名',
+                          `region_id` bigint(20) NOT NULL COMMENT '区域id',
+                          `region_name` varchar(40) NOT NULL COMMENT '区域名',
+                          `community_name` varchar(40) NOT NULL COMMENT '社区名',
+                          `detail_address` varchar(255) NOT NULL COMMENT '详细地址',
+                          `longitude` decimal(10, 7) NOT NULL COMMENT '经度',
+                          `latitude` decimal(10, 7) NOT NULL COMMENT '纬度',
+                          PRIMARY KEY (`id`) USING BTREE,
+                          INDEX `idx_user_id`(`user_id`) USING BTREE,
+                          INDEX `idx_rent_type`(`rent_type`) USING BTREE,
+                          INDEX `idx_title`(`title`) USING BTREE,
+                          INDEX `idx_community_name`(`community_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1000001 CHARACTER SET = utf8mb4 comment = '房源表';
+
+DROP TABLE IF EXISTS `house_status`;
+CREATE TABLE `house_status`  (
+                                 `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+                                 `house_id` bigint(20) NOT NULL COMMENT '房源id',
+                                 `status` varchar(10) NOT NULL COMMENT '房源状态',
+                                 `rent_time_code` varchar(20) NULL DEFAULT NULL COMMENT '出租时长（字典配置）',
+                                 `rent_start_time` bigint(20) NULL DEFAULT NULL COMMENT '开始出租时间（时间戳）',
+                                 `rent_end_time` bigint(20) NULL DEFAULT NULL COMMENT '结束出租时间（时间戳）',
+                                 PRIMARY KEY (`id`) USING BTREE,
+                                 UNIQUE INDEX `uk_house_id`(`house_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1000001 CHARACTER SET = utf8mb4 comment = '房源状态表';
+
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag`  (
+                        `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+                        `tag_code` varchar(20) NOT NULL COMMENT '标签码',
+                        `tag_name` varchar(20) NOT NULL COMMENT '标签名',
+                        PRIMARY KEY (`id`) USING BTREE,
+                        UNIQUE INDEX `uk_tag_code`(`tag_code`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1000001 CHARACTER SET = utf8mb4 comment = '标签表';
+
+
+DROP TABLE IF EXISTS `tag_house`;
+CREATE TABLE `tag_house`  (
+                              `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+                              `tag_code` varchar(20) NOT NULL COMMENT '标签码',
+                              `house_id` bigint(20) NOT NULL COMMENT '房源id',
+                              PRIMARY KEY (`id`) USING BTREE,
+                              INDEX `idx_tag_code`(`tag_code`) USING BTREE,
+                              INDEX `idx_house_id`(`house_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1000001 CHARACTER SET = utf8mb4 comment = '标签房源关系表';
+
+DROP TABLE IF EXISTS `city_house`;
+CREATE TABLE `city_house`  (
+                               `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+                               `city_id` bigint(20) NOT NULL COMMENT '城市id',
+                               `city_name` varchar(40) NOT NULL COMMENT '城市名',
+                               `house_id` bigint(20) NOT NULL COMMENT '房源id',
+                               PRIMARY KEY (`id`) USING BTREE,
+                               INDEX `idx_city_id`(`city_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1000001 CHARACTER SET = utf8mb4 comment = '城市房源关系表';
+
+INSERT INTO sys_dictionary_type (type_key,value,remark,status) VALUES
+                                                                   ('device_list','设备列表','房屋设备',1),
+                                                                   ('position_list','方向列表','房屋朝向',1),
+                                                                   ('rent_type_list','租房类型列表','',1),
+                                                                   ('rent_range','租金','租金范围',1),
+                                                                   ('rent_type','租房类型','房屋出租类型',1),
+                                                                   ('room_num','居室','房源居室数量',1),
+                                                                   ('rent_time','租期','房屋出租时长',1);
+
+INSERT INTO sys_dictionary_data (type_key,data_key,value,remark,sort,status) VALUES
+                                                                                 ('device_list','soft','沙发','',1,1),
+                                                                                 ('device_list','washer','洗衣机','',2,1),
+                                                                                 ('device_list','heater','热水器','',3,1),
+                                                                                 ('device_list','broadband','宽带','',4,1),
+                                                                                 ('device_list','icebox','冰箱','',5,1),
+                                                                                 ('device_list','hood','油烟机','',6,1),
+                                                                                 ('device_list','gas','燃气灶','',7,1),
+                                                                                 ('device_list','cook','可做饭','',8,1),
+                                                                                 ('device_list','tv','电视','',9,1),
+                                                                                 ('device_list','aircondition','空调','',10,1);
+INSERT INTO sys_dictionary_data (type_key,data_key,value,remark,sort,status) VALUES
+                                                                                 ('device_list','wardrobe','衣柜','',11,1),
+                                                                                 ('device_list','bed','床','',12,1),
+                                                                                 ('device_list','toilet','卫生间','',13,1),
+                                                                                 ('device_list','smartlock','智能门锁','',14,1),
+                                                                                 ('device_list','balcony','阳台','',15,1),
+                                                                                 ('device_list','heating','暖气','',16,1),
+                                                                                 ('position_list','east','东','',1,1),
+                                                                                 ('position_list','south','南','',2,1),
+                                                                                 ('position_list','west','西','',3,1),
+                                                                                 ('position_list','north','北','',4,1);
+INSERT INTO sys_dictionary_data (type_key,data_key,value,remark,sort,status) VALUES
+                                                                                 ('rent_type_list','whole_rent','整租','',1,1),
+                                                                                 ('rent_type_list','share_rent','合租','',2,1),
+                                                                                 ('rent_type_list','worry_free_rental','省心租','',3,1),
+                                                                                 ('rent_type_list','apartment','公寓','',4,1),
+                                                                                 ('rent_type_list','personal_house','个人房源','',4,1),
+                                                                                 ('rent_range','range_1','<1000元','',1,1),
+                                                                                 ('rent_range','range_2','1000-1500元','',2,1),
+                                                                                 ('rent_range','range_3','1500-2000元','',3,1),
+                                                                                 ('rent_range','range_4','2000-3000元','',4,1),
+                                                                                 ('rent_range','range_5','3000-5000元','',5,1);
+INSERT INTO sys_dictionary_data (type_key,data_key,value,remark,sort,status) VALUES
+                                                                                 ('rent_range','range_6','>5000元','',6,1),
+                                                                                 ('rent_type','whole','整租','',2,1),
+                                                                                 ('rent_type','shared','合租','',3,1),
+                                                                                 ('rent_type','trouble_free','省心租','',4,1),
+                                                                                 ('rent_type','apartment','公寓','',5,1),
+                                                                                 ('rent_type','individual','个人房源','',6,1),
+                                                                                 ('room_num','one','1居','',2,1),
+                                                                                 ('room_num','two','2居','',3,1),
+                                                                                 ('room_num','three','3居','',4,1),
+                                                                                 ('room_num','four','4居','',5,1);
+INSERT INTO sys_dictionary_data (type_key,data_key,value,remark,sort,status) VALUES
+                                                                                 ('room_num','more','4居以上','',6,1),
+                                                                                 ('rent_time','thirty_seconds','30秒','',1,1),
+                                                                                 ('rent_time','half_year','半年','',2,1),
+                                                                                 ('rent_time','one_year','一年','',3,1);
+INSERT INTO tag (tag_code,tag_name) VALUES
+                                        ('near_subway','近地铁'),
+                                        ('civil_water_elec','民水民电'),
+                                        ('lift','有电梯'),
+                                        ('central_heating','集中供暖'),
+                                        ('renovation','精装修'),
+                                        ('cook','可做饭');
